d4d919b05c7c0e89fa88a80ad891ac9205586bcc
Gayoung Kim <qrkdudq4301@naver.com>
2018-07-09 11:37:50 +0900

    Add new file: Ch3_1.hs

--- ch3/myproject/src/Ch3_1.hs
+++ ch3/myproject/src/Ch3_1.hs
@@ -16,6 +16,8 @@ charList = ['a', 'b', 'c', 'd', 'e', 'f']
 -- "abcdef"
 
 opList = [(+), (-), (*)]
+-- *Main Ch3_1 Lib> :t opList
+-- opList :: [Integer -> Integer -> Integer]  
 
 strList2 = ["abc", [], "ghi"]
 -- *Ch3_1> ["abc", [], "ghi"]
@@ -40,3 +42,35 @@ bool2 = [1, 2, 3] == [3, 2, 1]
 bool3 = [1 .. 5] == [1, 2 .. 5]
 -- *Ch3_1> [1 .. 5] == [1, 2 .. 5]
 -- True
+
+--chapter 2.8 exercise
+-- subst f g x = f x (g x)
+-- subst :: a -> b -> c -> d
+-- f :: (c -> e) -> d
+-- g :: c -> e
+-- x :: c
+
+-- a = e -> d
+-- b = c -> e
+-- c = c
+
+-- subst :: (c -> e -> d) -> (c -> e) -> c -> d
+
+trips [] = []
+trips [_] = []
+trips [_, _] = []
+trips xs =  let list3 = take 3 xs
+                tup = (list3 !! 0, list3 !! 1, list3 !! 2)
+            in [tup] ++ trips (tail xs)
+
+-- trips
+-- *Ch3_1> trips []
+-- []
+-- *Ch3_1> trips [1]
+-- []
+-- *Ch3_1> trips [1, 2]
+-- []
+-- *Ch3_1> trips [1, 2, 3]
+-- [(1,2,3)]
+-- *Ch3_1> trips [1, 2, 3, 4, 5]
+-- [(1,2,3),(2,3,4),(3,4,5)]
